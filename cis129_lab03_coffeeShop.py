412e7610fd75a2723926c4344e778deb95fbf155
AngelBatz <160679979+AngelBatz@users.noreply.github.com>
2024-02-28 22:58:45 -0700

    cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,1 +1,31 @@
+# asking the user how many coffees and muffins they would like to purchase
+numberOfCoffees = int(input('How many coffees would you like to purchase?'))
+numberOfMuffins = int(input('How many muffins would you like to purchase?'))
 
+# making the receipt based on what user inputed
+print('***************************************')
+print('My Coffee and Muffin Shop')
+print('Number of Coffees bought?')
+print(numberOfCoffees)
+print('***************************************')
+print('Number of Muffins bought?')
+print(numberOfMuffins)
+
+# Mutplying items bought with their prices
+print('***************************************')
+print('My Coffee and Muffin Shop Recipt')
+print(str(numberOfCoffees) + ' Coffees at $5 each: $' + str(numberOfCoffees * 5) )
+print(str(numberOfMuffins) + ' Muffins at $4 each: $' + str(numberOfMuffins * 4) )
+
+# setting variables to show taxes
+coffeeCost = numberOfCoffees * 5 
+muffinCost = numberOfMuffins * 4
+totalCost = coffeeCost + muffinCost
+tax = totalCost * 0.06
+totalCostWithTax = totalCost + tax
+
+# final portion of recipt 
+print(str('6% tax: $') + str(tax))
+print('---------')
+print(str('Total: $') + str(totalCostWithTax))
+print('***************************************')
